1204a52b4619678b6a4c6fd77af3c1c1423b30f9
Mickael Mignard <mignard.mickael@gmail.com>
2021-05-20 11:05:02 +0200

    Boucle de contrôle et affichage du résultat

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+function calculerAutorisation(){
+    var age = document.getElementById("age").value;   
+
+
+    if(!Number.isInteger(parseInt(age))){
+        alert("Veuillez saisir un nombre !")
+        return;
+    }
+
+    if(age >= 18){
+        return "Vous êtes majeur";
+    }else{
+        return "Vous êtes mineur";
+    }     
+}
+
+function afficherAutorisation(){
+    document.getElementById('resultat').innerHTML = calculerAutorisation();    
+}
